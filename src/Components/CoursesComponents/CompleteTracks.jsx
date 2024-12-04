6449ead3953ce479744a8f70080a1773ec77e9ab
anaswail <anaswail246@gmail.com>
2024-12-04 13:38:35 +0200

    Creative AMO

--- src/Components/CoursesComponents/CompleteTracks.jsx
+++ src/Components/CoursesComponents/CompleteTracks.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CompleteTracks = () => {
   return (
-    <div className="complete-tracks">
-      <div className="searchBar w-full text-center">
-        <input
-          className="p-3 text-xl w-96 rounded-2xl placeholder:text-sm outline-none my-10 "
-          type="text"
-          placeholder="Search for the trainer, language or genre..."
-        />
-      </div>
+    <div className="complete-tracks w-full flex justify-center items-center flex-col">
+      <h1 className="text-white font-bold text-5xl mt-16">
+        It will be added soon
+      </h1>
+      <Link
+        to="Courses/SeparateCourses"
+        className="text-[#ffac15] text-3xl mt-3 hover:text-yellow-600 "
+      >
+        Go to separate courses
+      </Link>
     </div>
   );
 };
